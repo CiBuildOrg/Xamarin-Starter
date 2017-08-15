cf5628bbe19a2137e0d0d63cbb40dce79e4bc912
Doru Cioclea <cioclea.doru@gmail.com>
2017-08-15 19:05:14 +0300

    add autofac and autofac mvx provider

---
+++ src/App.Template.XForms.Core/Autofac/Resources.cs
@@ -0,0 +1,7 @@
+namespace App.Template.XForms.Core.Autofac
+{
+    public class Resources
+    {
+        public const string TypeNotRegisteredAsSingleton = "Type '{0}' is not registered as a singleton.";
+    }
+}

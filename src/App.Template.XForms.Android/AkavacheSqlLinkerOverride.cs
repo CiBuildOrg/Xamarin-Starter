c006df78b806321c822ebbb4e59baa36bf693c56
Doru Cioclea <cioclea.doru@gmail.com>
2017-08-20 14:09:51 +0300

    added the linker overrides for akavache

---
+++ src/App.Template.XForms.Android/AkavacheSqlLinkerOverride.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Akavache.Sqlite3;
+
+namespace App.Template.XForms.Android
+{
+    [Preserve]
+    public static class LinkerPreserve
+    {
+        static LinkerPreserve()
+        {
+            throw new Exception(typeof(SQLitePersistentBlobCache).FullName);
+        }
+    }
+
+
+    public class PreserveAttribute : Attribute
+    {
+    }
+}
---
+++ src/App.Template.XForms.iOS/AkavacheSqlLinkerOverride.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Akavache.Sqlite3;
+using Foundation;
+using UIKit;
+
+namespace App.Template.XForms.iOS
+{
+    [Preserve]
+    public static class LinkerPreserve
+    {
+        static LinkerPreserve()
+        {
+            throw new Exception(typeof(SQLitePersistentBlobCache).FullName);
+        }
+    }
+
+
+    public class PreserveAttribute : Attribute
+    {
+    }
+}

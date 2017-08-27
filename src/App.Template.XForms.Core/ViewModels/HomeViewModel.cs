9dc5697165aaec12038cbee76d3df92156f6bf21
Doru Cioclea <cioclea.doru@gmail.com>
2017-08-27 18:34:55 +0300

    fix scan barcode command

--- src/App.Template.XForms.Core/ViewModels/HomeViewModel.cs
+++ src/App.Template.XForms.Core/ViewModels/HomeViewModel.cs
@@ -1,17 +1,27 @@
-﻿using App.Template.XForms.Core.ViewModels.Base;
+﻿using System.Diagnostics.CodeAnalysis;
+using App.Template.XForms.Core.ViewModels.Base;
 using MvvmCross.Core.Navigation;
+using MvvmCross.Core.ViewModels;
 
 namespace App.Template.XForms.Core.ViewModels
 {
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
     public class HomeViewModel : BasePageViewModel
     {
-        public void ScanBarcode()
-        {
-            ShowViewModel<ScanBarcodeViewModel>();
-        }
-
         public HomeViewModel(IMvxNavigationService navigationService) : base(navigationService)
         {
+
+        }
+
+        private IMvxCommand _scanBarcodeCommand;
+
+        [SuppressMessage("ReSharper", "UnusedMember.Global")]
+        public IMvxCommand ScanBarcode => _scanBarcodeCommand ??
+                                          (_scanBarcodeCommand = new MvxCommand(ScanBarcodeImplementation));
+
+        private void ScanBarcodeImplementation()
+        {
+            ShowViewModel<ScanBarcodeViewModel>();
         }
     }
 }
